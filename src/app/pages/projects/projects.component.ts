3b196cc1aa92777b38b79efe1f3a5d2210ecfbed
a_dankova <a_dankova@werbeagentur-maier.com>
2021-05-29 15:32:27 +0200

    accordion

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -14,6 +14,20 @@ export class ProjectsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
   }
 
 }
